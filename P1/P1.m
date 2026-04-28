0f9fc80790172b8fcacf55a9f441ce3aaa670f7d
Luís Almeida <luispma@NULL.Home>
2026-04-28 18:29:33 +0100

    Updates to P1

--- P1/P1.m
+++ P1/P1.m
@@ -1,5 +1,26 @@
-x1min = -2; x1max = 2;
-x2min = -1; x2max = 3;
+% Illustrates the unconstrained and constrained minimization of the 
+% Rosenbrock function following the structure of ProbBasic.m.
+%
+% Matlab sw required: optimization toolbox
+% Functions called
+%    fminunc    - Matlab function for unconstrained minimization
+%    fmincon    - Matlab function for constrained minimization
+%    Rosenbrock - user defined function to be minimized
+%
+% IST, MEEC, Distributed Predictive Control and Estimation
+% Afonso Botelho, Joao Miranda Lemos, 2025
+%--------------------------------------------------------------------------
+
+%--------------------------------------------------------------------------
+% Plots function level curves
+
+% Range of independent variables to consider (Adjusted for Rosenbrock)
+x1min = -2;
+x1max = 2;
+x2min = -0.5;
+x2max = 3.5;
+
+% Number of intervals in the mesh grid
 N1 = 100;
 N2 = 100;
 
@@ -7,71 +28,78 @@ xv1 = linspace(x1min, x1max, N1);
 xv2 = linspace(x2min, x2max, N2);
 [xx1, xx2] = meshgrid(xv1, xv2);
 
+% Computes the function at the different points of the mesh grid
 for ii = 1:N1
     for jj = 1:N2
         x = [xx1(ii,jj); xx2(ii,jj)];
-        ff(ii,jj) = RosenbrockFunction(x);
+        ff(ii,jj) = Rosenbrock(x);
     end
 end
 
+% Plots the level curves using the Matlab function contour
+Nlevel = 20;  
+LW = 'linewidth'; FS = 'fontsize'; MS = 'markersize';
+figure(1), contour(xv1, xv2, ff, Nlevel, LW, 1.2), colorbar
+axis([x1min x1max x2min x2max]), axis square
+hold on
+
+%--------------------------------------------------------------------------
+% Compute the minimum
+
+% Initial estimate of the minimum
 x0 = [-1; 1];
 
+% Define the options to be used with the fminunc solver
 options = optimoptions('fminunc', 'Algorithm', 'quasi-newton');
 
-xopt = fminunc(@RosenbrockFunction, x0, options);
+% Uses the solver fminunc to compute the unconstrained minimum
+xopt = fminunc(@Rosenbrock, x0, options);
+
+%--------------------------------------------------------------------------
+% Computes the constrained minimum associated to the constraint x1 <= 0.5
+% Written as Ac*x <= Bc
 
 Ac = [1 0];
 Bc = 0.5;
-xoptconstr = fmincon(@RosenbrockFunction, x0, Ac, Bc);
+xoptconstr = fmincon(@Rosenbrock, x0, Ac, Bc);
 
 %--------------------------------------------------------------------------
-% Figure 1: level curves with initial estimate, unconstrained and
-% constrained minima
+% Plots markers (using the same red color style as ProbBasic.m)
 
-Nlevel = 30;
-figure(1)
-contour(xv1, xv2, ff, Nlevel, 'LineWidth', 1.2)
-colorbar
-axis([x1min x1max x2min x2max])
-axis square
-hold on
-
-plot([0.5 0.5], [x2min x2max], 'k--', 'LineWidth', 1.5)
-
-% Plot initial estimate marked with "o"
+% Initial point as a red circle
 gg = plot(x0(1), x0(2), 'or');
-set(gg, 'LineWidth', 2, 'MarkerSize', 12)
+set(gg, 'Linewidth', 1.5);
 
-% Plot unconstrained minimum marked with "x"
-gg = plot(xopt(1), xopt(2), 'xb');
-set(gg, 'LineWidth', 2, 'MarkerSize', 12)
+% Unconstrained minimum as a red cross
+gg = plot(xopt(1), xopt(2), 'xr');
+set(gg, 'Linewidth', 1.5);
 
-% Plot constrained minimum marked with "*"
-gg = plot(xoptconstr(1), xoptconstr(2), '*g');
-set(gg, 'LineWidth', 2, 'MarkerSize', 12)
+% Constrained minimum as a red star
+gg = plot(xoptconstr(1), xoptconstr(2), '*r');
+set(gg, 'Linewidth', 1.5);
 
-gg = xlabel('x_1');
-set(gg, 'FontSize', 14)
-gg = ylabel('x_2');
-set(gg, 'FontSize', 14)
-title('Rosenbrock function - level curves', 'FontSize', 14)
+% Plots the constraint boundary (x1 = 0.5)
+z2c = linspace(x2min, x2max, 100);
+z1c = 0.5 * ones(size(z2c));
+gg = plot(z1c, z2c, 'k');
+set(gg, 'Linewidth', 1.5);
+
+% Identifies axis
+gg = xlabel('x_1'); set(gg, 'FontSize', 14);
+gg = ylabel('x_2'); set(gg, 'FontSize', 14);
 
 hold off
 
 %--------------------------------------------------------------------------
-% Figure 2: 3D view of the Rosenbrock function
+% Plots the 3d view of the function with the square grid
 
 figure(2)
-surf(xx1, xx2, ff)
-shading interp
+surf(xx1, xx2, ff); % Standard surf creates the 'squares grid'
 
-gg = xlabel('x_1');
-set(gg, 'FontSize', 14)
-gg = ylabel('x_2');
-set(gg, 'FontSize', 14)
-gg = zlabel('f(x)');
-set(gg, 'FontSize', 14)
-title('Rosenbrock function - 3D view', 'FontSize', 14)
+% Identifies axis
+gg = xlabel('x_1'); set(gg, 'FontSize', 14);
+gg = ylabel('x_2'); set(gg, 'FontSize', 14);
+gg = zlabel('f(x)'); set(gg, 'FontSize', 14);
 
-fprintf('Unconstrained minimum: x1 = %.4f, x2 = %.4f\n', xopt(1), xopt(2))
-fprintf('Constrained minimum:   x1 = %.4f, x2 = %.4f\n', xoptconstr(1), xoptconstr(2))
+%--------------------------------------------------------------------------
+% End of File
--- P1/RosenbrockFunction.m
+++ P1/RosenbrockFunction.m
@@ -1,3 +1,18 @@
-function f = RosenbrockFunction(x)
-    f = 100*(x(2)-x(1)^2)^2 + (1-x(1))^2;
+function f = Rosenbrock(x)
+%   This function computes the Rosenbrock function in 2 dimensions at a point.
+%   The argument x of the function is a 2-dimensional vector;
+%   The function computes the value of the Rosenbrock function at x.
+%
+%   f(x1, x2) = 100*(x2 - x1^2)^2 + (1 - x1)^2
+%
+%   The global (unconstrained) minimum is at x* = [1; 1] with f(x*) = 0.
+%
+%   IST - MEEC - Distributed Predictive Control and Estimation
+%   Afonso Botelho, Joao Miranda Lemos, 2025
+%--------------------------------------------------------------------------
+
+f = 100*(x(2) - x(1)^2)^2 + (1 - x(1))^2;
+
 end
+%--------------------------------------------------------------------------
+% End of file
